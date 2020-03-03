c9ddce4af91338862331ae753804da57852b313a
Dmitry Savransky <dsavransky@gmail.com>
2020-03-03 13:35:30 -0500

    lissajous orbit update

--- CR3BP/lissajous_orbits.m
+++ CR3BP/lissajous_orbits.m
@@ -19,6 +19,33 @@ for j = 1:length(l)
         if k == 1
             zlabel(['\lambda = ',num2str(l(j))])
         end
+        grid on
+        counter = counter + 1;
+        
+    end
+end
+
+nu = sqrt([2,3,5,6]);
+
+t = linspace(0,6*pi,1000);
+figure(2)
+clf
+counter = 1;
+for j = 1:length(l)
+    for k = 1:length(nu)
+        subplot(length(l),length(nu),counter)
+        x = cos(l(j)*t);
+        y = sin(l(j)*t);
+        z = sin(nu(k)*t);
+        plot3(x,y,z)
+        set(gca,'XTickLabel',{},'YTickLabel',{},'ZTickLabel',{},'FontSize',16)
+        if j == 1
+            title(['$\nu = \sqrt{',num2str(nu(k)^2),'}$'],'Interpreter','latex')
+        end
+        if k == 1
+            zlabel(['\lambda = ',num2str(l(j))])
+        end
+        grid on
         counter = counter + 1;
         
     end
